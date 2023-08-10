4a6d7958fa7974ae5d2bed8cdc6a8bff8e36c1f6
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-10 04:58:33 -0400

    Instalación de bootstrap con npm
    Ya que se están usando los manejadores de dependencias, en este caso composer para backend php y se requería bootstrap que es frontend, se implementó npm igual que en laravel, se inició el package.json y se instaló bootstrap, luego se compiló utilizando saas para los css y se llaman los scripts desde node_modules, también se modificó gitignore para ignorar node_modules

---
+++ resources/views/lists.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Listado de productos</title>
+
+    <link rel="stylesheet" href="./../../public/assets/css/app.css">
+  </head>
+  <body>
+    
+
+    <script src="./../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
