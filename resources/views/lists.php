60cd05c302582be0ea28573dbf1092c0f3d92e19
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-10 05:13:34 -0400

    Vista de productos terminada
    Se creó el diseño de la vista para iterar los productos y darle estilo con bootstrap

--- resources/views/lists.php
+++ resources/views/lists.php
@@ -8,7 +8,27 @@
     <link rel="stylesheet" href="./../../public/assets/css/app.css">
   </head>
   <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Listado de productos</h1>
     
+                <?php 
+                foreach ($products as $product) {
+                    echo "
+                    <span class='fw-bold'>{$product->name}</span> - {$product->description} <br>
+                    ";
+                }
+                ?>
+
+                <hr>
+
+                <div>
+                    <a href="./pdf.php">Exportar en PDF</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="./../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   </body>
