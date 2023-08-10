c595c30aafc0816c3e3dd9df046472c5f2a9ecdd
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-10 05:58:15 -0400

    Corrección de rutas
    La vista lists se encuentra en resources/views
    Pero al ser incluida en public/index se deben especificar los llamados desde ese origen

--- resources/views/lists.php
+++ resources/views/lists.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Listado de productos</title>
 
-    <link rel="stylesheet" href="./../../public/assets/css/app.css">
+    <link rel="stylesheet" href="./assets/css/app.css">
   </head>
   <body>
     <div class="container">
@@ -24,12 +24,12 @@
                 <hr>
 
                 <div>
-                    <a href="./pdf.php">Exportar en PDF</a>
+                    <a href="./pdf.php" class="btn btn-outline-primary">Exportar en PDF</a>
                 </div>
             </div>
         </div>
     </div>
 
-    <script src="./../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
